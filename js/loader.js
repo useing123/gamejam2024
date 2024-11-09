75f017fb058317b7de6b0666e6f4000b687fd9f7
Movchan17 <ivanmovchan17@gmail.com>
2024-11-10 01:44:39 +0500

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,10 @@
 document.body.onload = function(){
     setTimeout(function(){
-        let loader = document.querySelector('.loader');
+       let loader = document.querySelector('.loader');
         let container = document.querySelector('.container');
         if(!loader.classList.contains('off_loader')){
             loader.classList.add('off_loader');
             container.style.display = 'block';
         };
-    }, 6000);
+    }, 0);
 };
